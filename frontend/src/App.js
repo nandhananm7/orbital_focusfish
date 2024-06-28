05927cd9ba926ee5f4d3c2254fdc4d469643f9fc
chuanhuiw <142211269+chuanhuiw@users.noreply.github.com>
2024-06-28 15:42:01 +0800

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Main from './components/Main';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Pomodoro from './components/Pomodoro';
+import Todo from './components/Todo';
+
+
+function App() {
+  const user = localStorage.getItem("token")
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main/>}/>}
+      <Route path="/signup" exact element={<Signup/>}/>
+      <Route path="/login" exact element={<Login/>}/>
+      <Route path="/pomodoro" exact element={<Pomodoro/>}/>
+      <Route path="/todo" exact element={<Todo/>}/>
+      <Route path="/main" exact element={<Main/>}/>
+      <Route path="/" exact element={<Navigate replace to="/login"/>}/>
+    </Routes>
+  );
+}
+
+export default App;
